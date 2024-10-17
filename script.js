fddb9fb19bfc03f235283cf320a14a4b20784983
Bahaa Mohammed <45239532+bahaamohammed@users.noreply.github.com>
2024-10-17 12:18:12 +0300

    add milliseconds

--- script.js
+++ script.js
@@ -1,29 +1,34 @@
 let time = document.getElementById("time");
-let [seconds, minutes, hours] = [0, 0, 0];
+let [milliseconds, seconds, minutes, hours] = [0, 0, 0, 0];
 let timer = null;
 
 function stopWatch(){
-    seconds++;
-    if(seconds == 60){
-        seconds = 0;
-        minutes++;
-        if(minutes == 60){
-            minutes = 0;
-            hours++;
+    milliseconds += 10; // Update by 10ms for precision
+    if (milliseconds == 1000) { // 1000 milliseconds equals 1 second
+        milliseconds = 0;
+        seconds++;
+        if (seconds == 60) {
+            seconds = 0;
+            minutes++;
+            if (minutes == 60) {
+                minutes = 0;
+                hours++;
+            }
         }
     }
 
     let h = hours < 10 ? "0" + hours : hours;
     let m = minutes < 10 ? "0" + minutes : minutes;
     let s = seconds < 10 ? "0" + seconds : seconds;
-    time.innerHTML = h + ":" + m + ":" + s;
+    let ms = milliseconds < 100 ? (milliseconds < 10 ? "00" + milliseconds : "0" + milliseconds) : milliseconds;
+    time.innerHTML = h + ":" + m + ":" + s + ":" + ms;
 }
 
 function watchStart(){
     if(timer != null){
         clearInterval(timer);
     }
-    timer = setInterval(stopWatch, 1000);
+    timer = setInterval(stopWatch, 10); // Set to update every 10ms
 }
 
 function watchStop(){
@@ -32,6 +37,6 @@ function watchStop(){
 
 function watchReset(){
     clearInterval(timer);
-    [seconds, minutes, hours] = [0, 0, 0];
-    time.innerHTML = "00:00:00";
+    [milliseconds, seconds, minutes, hours] = [0, 0, 0, 0];
+    time.innerHTML = "00:00:00:000"; // Update to show milliseconds
 }
